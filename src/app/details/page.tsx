2b91df3a3802696c676b399eabd1736a0ea0a20b
Ričardas Jaksebaga <ricardas.jaksebaga@gmail.com>
2023-05-18 22:52:35 +0200

    Added external api integration for Details page
    * added anchor links

--- src/app/details/page.tsx
+++ src/app/details/page.tsx
@@ -1,11 +1,20 @@
 import Link from 'next/link';
 
 export const revalidate = 0;
-export default function DetailsPage() {
+export default async function DetailsPage() {
+    const timeApiResp = await fetch(
+        'https://timeapi.io/api/Time/current/zone?timeZone=Europe/Amsterdam',
+        {
+            next: { revalidate: 0 },
+            cache: 'no-cache',
+        });
     return (
         <div>
-            <h1>Details page, server time: {new Date().toLocaleTimeString()}</h1>
+            <h1>Details page</h1>
+            <h2>Time in Amsterdam: {(await timeApiResp.json()).dateTime}</h2>
             <Link href={'/'} prefetch={false}>Back</Link>
+            <br/><br/>
+            <a href={'/'}>Back (anchor tag)</a>
         </div>
-    )
+    );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import Link from 'next/link';
 
 export const revalidate = 0;
+export const dynamic = 'force-dynamic';
 export default function Home() {
-  return (
-      <div>
-        <h1>Home page, server time: {new Date().toLocaleTimeString()}</h1>
-        <Link href={'/details'} prefetch={false}>Details page</Link>
-    </div>
-  )
+    return (
+        <div>
+            <h1>Home page</h1>
+            <h2>server time: {new Date().toLocaleTimeString()}</h2>
+            <Link href={`/details`} prefetch={false}>Amsterdam time</Link>
+            <br/><br/>
+            <a href={'/details'}>Amsterdam time (anchor tag)</a>
+        </div>
+    );
 }
